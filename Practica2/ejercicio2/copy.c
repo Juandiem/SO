d9e7ca1483cd63879507162a975183175989b827
juandiem <juandiem@ucm.es>
2022-09-29 12:51:09 +0200

    Ej2 casi

--- Practica2/ejercicio2/copy.c
+++ Practica2/ejercicio2/copy.c
@@ -1,13 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <err.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 
 void copy(int fdo, int fdd)
 {
+	char c[64];
+	int count;
+	/* Read file byte by byte */
+	while (count = read(&fdo, c, 64)) {
+		/* Print byte to stdout */
+		if (!write(fdd, c, count)){
+			close(fdo);close(fdd);
+			err(3,"fwrite failed!!");
+			}
+		}
 
+	close(fdo);close(fdd);
 }
+
+
 
 int main(int argc, char *argv[])
 {
+	int fdo, fdd;
+
+	if (argc!=2) {
+		fprintf(stderr,"Usage: %s <sourcefile_name>\n",argv[0]);
+		exit(1);
+	}
+	else if (argc!=3) {
+		fprintf(stderr,"Usage: %s <destfile_name>\n",argv[0]);
+		exit(1);
+	}
+
+	if((fdo = open(argv[1], O_RDONLY, 0444)) && (fdd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0444)))
+		copy(fdo,fdd);
+	else
+		err(2,"The input file %s could not be opened",argv[1]);
+
 
 	return 0;
 }
