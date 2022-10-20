780a9615728c6353bcf17fb0a46fec81e0b1c50d
Juandiem <juandiem@ucm.es>
2022-10-20 12:52:12 +0200

    ej1 p3

---
+++ Practica3/ejercicio1/fork1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*programa que crea dos hijos: uno no cambia de ejecutable y otro si */
+
+
+int main(int argc, char **argv)
+{
+	pid_t p_ID;
+	pid_t child_ID;
+
+	if (argc!=1) {
+		fprintf(stderr,"Usage: %s <exe_name> <newExe_name> <chuakala>\n",argv[0]);
+		exit(1);
+	}
+
+	p_ID = getpid();
+	printf("proc. PID = %d comienza \n", p_ID);
+	child_ID = fork();
+	printf("proc. PID = %d, pidC = %d \n", p_ID, child_ID);
+
+
+	if(child_ID > 0){
+		p_ID = getpid();
+		printf("proc. PID = %d sigue \n", p_ID);
+		child_ID = fork();
+		printf("proc. PID = %d, pidC = %d \n", p_ID, child_ID);
+	}
+	
+	if(child_ID > 0){
+		const char *args_[] = {NULL};
+		execvp(argv[1], args_);
+		waitpid(getppid(),-1, 0);
+	}
+
+	return 0;
+}
